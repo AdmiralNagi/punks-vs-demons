397c227dd815eb4e4c1a89a28b20d9616a43ca54
sapbrown <sapbrown@knights.ucf.edu>
2018-09-09 12:38:21 -0400

    Created Lanes and BPM tester
    Currently sends out one random note every beat. You can change the BPM and it will change the rate of notes.
    
    Known Issue: when the last note is spawned, there is a skip in the metronome.

--- Punks vs Demons/Assets/Scripts/SongManager.cs
+++ Punks vs Demons/Assets/Scripts/SongManager.cs
@@ -1,27 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SongManager : MonoBehaviour {
 
+	public AudioSource metronomeClick;
+	public Transform[] originPositions;
+	public Transform[] endPositions;
 	//current position in seconds
 	float trackPosistion;
 	//current position in beats
-	float trackPosInBeats;
+	public float trackPosInBeats;
 	//duration of a beat
 	float secPerBeat;
 	//time in seconds since start of track
 	float dsptimesong;
+	//how many beats are previewed
+	public float beatsShownEarly;
+
+	public float bpm;
+	public float[] notes;
+	public int noteIndex=0;
+
+	//prototyping purposes
+	public float trackLengthInMin;
+	public GameObject note;
 
 	// Use this for initialization
 	void Start () {
-//		secPerBeat = 60f / bpm;
-//
-//		dsptimesong = (float)AudioSettings.dspTime;
+		secPerBeat = 60f / bpm;
+		beatsShownEarly = 10;
+
+		dsptimesong = (float)AudioSettings.dspTime;
+
+		//prototyping purposes
+		notes = new float[(int)(bpm*trackLengthInMin)];
+		for (int x = 0; x < notes.Length; x++) {
+			notes [x] = (float)(x) + beatsShownEarly;
+		}
+		//
+
+		//GetComponent<AudioSource> ().Play ();
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
-		
+		trackPosistion = (float)(AudioSettings.dspTime - dsptimesong);
+
+		if (noteIndex < notes.Length && notes [noteIndex] < trackPosInBeats + beatsShownEarly) {
+			Instantiate (note);
+
+			noteIndex++;
+		}
+
+		trackPosInBeats = trackPosistion / secPerBeat;
 	}
 }
---
+++ Punks vs Demons/Assets/Scripts/TrackNote.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class TrackNote : MonoBehaviour {
+
+	public SongManager songManager;
+	//arrays of note colors, starting points, and end points
+	public Material[] noteColors;
+
+	//index number of the current color
+	private int whatColorIndex;
+	Vector3 originPos;
+	Vector3 endPos;
+	float beatsShownEarly;
+	float beatOfThisNote;
+	float trackPosInBeats;
+
+	// Use this for initialization
+	void Start () {
+		songManager = GameObject.Find ("SongManager").GetComponent<SongManager> ();
+		beatsShownEarly = songManager.beatsShownEarly;
+		if(songManager.noteIndex < songManager.notes.Length)
+			beatOfThisNote = songManager.notes [songManager.noteIndex];
+		//currently this is random, but this will change to hard programmed after track editor is complete
+		whatColorIndex = Random.Range(0,noteColors.Length);
+		gameObject.GetComponent<Renderer> ().material = noteColors [whatColorIndex];
+
+		originPos = songManager.originPositions [whatColorIndex].position;
+		endPos = songManager.endPositions [whatColorIndex].position;
+		transform.position = originPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (originPos, endPos,
+			(beatsShownEarly - (beatOfThisNote - songManager.trackPosInBeats)) / beatsShownEarly);
+
+		if (transform.position.Equals (endPos)) {
+			songManager.metronomeClick.Play ();
+			Destroy (gameObject);
+		}
+	}
+}
