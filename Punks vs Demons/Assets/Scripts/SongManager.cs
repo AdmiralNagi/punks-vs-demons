870b3b894380beaaaff586d6c43d98e3fb031a04
sapbrown <sapbrown@knights.ucf.edu>
2018-09-09 07:58:52 -0400

    File Structure and Setup Prototype
    I created a basic file structure, and started work on the logic of the prototype in a new scene as well as downloaded one track to test with.

---
+++ Punks vs Demons/Assets/Scripts/SongManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour {
+
+	//current position in seconds
+	float trackPosistion;
+	//current position in beats
+	float trackPosInBeats;
+	//duration of a beat
+	float secPerBeat;
+	//time in seconds since start of track
+	float dsptimesong;
+
+	// Use this for initialization
+	void Start () {
+//		secPerBeat = 60f / bpm;
+//
+//		dsptimesong = (float)AudioSettings.dspTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
