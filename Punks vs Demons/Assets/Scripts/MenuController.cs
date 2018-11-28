619ff052891d405cf9793f2aad4e415ecd3a6f38
sapbrown <sapbrown@outlook.com>
2018-11-27 21:01:03 -0500

    Stage2 adjustment and Main Menu implementation

--- Punks vs Demons/Assets/Scripts/MenuController.cs
+++ Punks vs Demons/Assets/Scripts/MenuController.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
+	[SerializeField]private SceneController sceneController;
 	private SongSettings songSettings;
 	public Button firstSong;
 	public Button secondSong;
+	public GameObject stage1;
+	public GameObject stage2;
+	public GameObject howToPlay;
 	public Text bpm;
 	public Text beatsPerMeasure;
 	public Text songButton;
+	public Text songTitle;
+
+	private string[ , ] songTitleArray = {{"\"STORM\"\nNORMCORE", "\"SONGTILE\"\nARTIST"},
+		{"\"PUNK FOR FASHION\"\nPSYCHOTIC REACTION","\"SONGTILE\"\nARTIST" }};
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +29,7 @@ public class MenuController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		songTitle.text = songTitleArray [sceneController.stage - 1, songTitleIndex];
 		bpm.text = songSettings.bpm.ToString ();
 		beatsPerMeasure.text = songSettings.beatsPerMeasure.ToString ();
 	}
@@ -50,33 +59,93 @@ public class MenuController : MonoBehaviour {
 	}
 
 	public void fashionSong(){
-		firstSong.image.color = Color.red;
-		secondSong.image.color = Color.white;
+//		firstSong.image.color = Color.red;
+//		secondSong.image.color = Color.white;
 		songSettings.songChosen = true;
 		songSettings.bpm = 200f;
 		songSettings.songIndex = 0;
 	}
 
 	public void stormSong(){
-		secondSong.image.color = Color.red;
-		firstSong.image.color = Color.white;
+//		secondSong.image.color = Color.red;
+//		firstSong.image.color = Color.white;
 		songSettings.songChosen = true;
-		songSettings.bpm = 106f;
+		songSettings.bpm = 105f;
 		songSettings.songIndex = 1;
+	}
+
+	public void StageOne(){
+		stage1.SetActive (true);
+		stage2.SetActive (false);
+		sceneController.stage = 1;
+	}
+
+	public void StageTwo(){
+		stage1.SetActive (false);
+		stage2.SetActive (true);
+		sceneController.stage = 2;
+	}
+
+	public void HowToPlay(){
+		howToPlay.SetActive (!howToPlay.activeInHierarchy);
+	}
+
+	public void SongOne(){
+		if (sceneController.stage == 1) {
+			stormSong ();
+		} else if (sceneController.stage == 2) {
+			fashionSong ();
+		}
+	}
+
+	public void SongTwo(){
+
+	}
+
+	private int songTitleIndex = 0;
+	public void RightArrow(){
+		if (songTitleIndex < songTitleArray.GetLength(sceneController.stage - 1) -1) {
+			songTitleIndex++;
+		} else {
+			songTitleIndex = 0;
+		}
+	}
+
+	public void LeftArrow(){
+		if (songTitleIndex > 0) {
+			songTitleIndex--;
+		} else {
+			songTitleIndex = songTitleArray.GetLength(sceneController.stage - 1) -1;
+		}
+	}
+
+	public void Rock(){
+		if (!jamOn) {
+			if (songTitleIndex == 0) {
+				SongOne ();
+			} else if (songTitleIndex == 1) {
+				SongTwo ();
+			}
+		}
+
+		sceneController.LoadScene ();
 	}
 
 	public GameObject practicePanel;
 	public Text practiceBtnText;
 	private float bpmTemp;
+	private bool jamOn = false;
 	public void ActivatePractice(){
 		songSettings.songChosen = !(songSettings.songChosen);
 		practicePanel.SetActive (!(songSettings.songChosen));
 		if (practicePanel.activeInHierarchy) {
+			jamOn = true;
 			songSettings.bpm = bpmTemp;
 			practiceBtnText.text = "BACK";
 		} else {
+			jamOn = false;
 			bpmTemp = songSettings.bpm;
-			songSettings.bpm = 200f;
+			songSettings.bpm = 120f;
 			practiceBtnText.text = "JAM";
 		}
 	}
